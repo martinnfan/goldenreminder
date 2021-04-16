78c0399f056a58e76c36df74df5a417a9646f2fc
martinnfan <54997981+martinnfan@users.noreply.github.com>
2021-04-16 00:45:27 -0400

    Created a checkbox list in products, need to create an ngIf to hide certain topics as a "filtering box"

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, FormControl, ValidatorFn } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { products } from './products';
 
@@ -11,11 +12,41 @@ export class ProductsComponent implements OnInit {
 
   products = products;
 
-  constructor(private route: ActivatedRoute) { }
+  // Instatiate form variables
+  form: FormGroup;
+  options = [
+    { id: 1, name: 'Earrings' },
+    { id: 2, name: 'Necklaces' },
+  ];
+
+  get ordersFormArray() {
+    return this.form.controls.orders as FormArray;
+  }
+
+  constructor(
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
+    ) {
+      this.form = this.formBuilder.group({
+        orders: new FormArray([])
+      });
+
+      this.addCheckboxes();
+     }
+
+  private addCheckboxes() {
+    this.options.forEach(() => this.ordersFormArray.push(new FormControl(false)));
+  }
 
   ngOnInit(): void {
   }
 
-  
+  submit() {
+    const selectedOrdersIds = this.form.value.orders
+      .map((checked, i) => checked ? this.options[i].id : null)
+      .filter(v => v!== null);
+    console.log(selectedOrdersIds);
+  }
+
 
 }
